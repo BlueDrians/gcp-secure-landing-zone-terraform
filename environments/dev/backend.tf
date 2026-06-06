bb56dfcabfecda4c9070b030f7250cff405476f7
BlueDrians <yadrianmaulana@gmail.com>
2026-06-06 14:59:20 +0700

    Update backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 # Recommended for team usage:
-# 1. Create a GCS bucket dedicated for Terraform state.
-# 2. Uncomment and update this backend block.
+# 1. Create a GCS bucket dedicated for Terraform state
+# 2. Uncomment and update this backend block
 #
 # terraform {
 #   backend "gcs" {
