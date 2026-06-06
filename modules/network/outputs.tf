fa5dbc24ccfee515e61a7b6fef53f9f4c316f4ca
BlueDrians <yadrianmaulana@gmail.com>
2026-06-06 15:02:42 +0700

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,19 +1,19 @@
 output "vpc_name" {
-  description = "VPC name."
+  description = "VPC name"
   value       = google_compute_network.main.name
 }
 
 output "vpc_id" {
-  description = "VPC ID."
+  description = "VPC ID"
   value       = google_compute_network.main.id
 }
 
 output "subnet_name" {
-  description = "Subnet name."
+  description = "Subnet name"
   value       = google_compute_subnetwork.private.name
 }
 
 output "subnet_id" {
-  description = "Subnet ID."
+  description = "Subnet ID"
   value       = google_compute_subnetwork.private.id
 }
